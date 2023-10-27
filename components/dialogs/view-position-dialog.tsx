c55b96ea8e3916ae43169ac3d759021b992e7616
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-10-27 10:40:52 +0000

    view position

--- components/dialogs/view-position-dialog.tsx
+++ components/dialogs/view-position-dialog.tsx
@@ -8,19 +8,37 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Position } from "@/types"
+import { convertDate } from "@/lib/utils"
 
-export default function ViewPositionDialog({data}: {data: Position}) {
-    
+export default function ViewPositionDialog({ data }: { data: Position }) {
+    const { _id, createdAt, description, title, updatedAt } = data
+
 
     return (
-            <>
-                <DialogHeader>
-                <DialogTitle className="poppins">Create a Position</DialogTitle>
-                    <DialogDescription>
-                        Create a new position for your executive team
-                    </DialogDescription>
-                </DialogHeader>
-            </>
+        <>
+            <DialogHeader>
+                <DialogTitle className="poppins">View {title} Position </DialogTitle>
+                <DialogDescription>
+                    {description}
+                </DialogDescription>
+            </DialogHeader>
+            <div className="w-full flex m flex-col text-sm gap-2">
+                <p className="text-xs uppercase">
+                    Created At
+                </p>
+                <p>
+                    {convertDate(createdAt)}
+                </p>
+            </div>
+            <div className="w-full flex m flex-col gap-2">
+                <p className="text-xs uppercase">
+                    Created At
+                </p>
+                <p>
+                    {convertDate(updatedAt)}
+                </p>
+            </div>
+        </>
     )
 }
 
